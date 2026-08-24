f3d40b6b9b62982673696f1a39cced193cfdcf9c
M-Maahir <maahir.m27@gmail.com>
2026-08-23 20:03:36 -0400

    new method

--- app/main.py
+++ app/main.py
@@ -10,8 +10,14 @@ def home():
         "message":"Maahir from feature branch one"
     }
 
-@app.get("/users")
-def users(user: dict):
+@app.get("/users/{user}")
+def users(user: str):
     return {
         "user":user
+    }
+
+@app.get('/names')
+def names():
+    return{
+        "names":"Maahir is one of the name"
     }
